addac6d9799b7263ef0c23194be916a535db246d
Jeff <jeff4f5da2@qq.com>
2025-08-21 20:46:11 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"net/http"
+	"powerquery/query"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	RodUrl string `mapstructure:"rod_url"`
+}
+
+func main() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	queryer, err := query.NewRodQueryer(viper.GetString("rod_url"))
+	if err != nil {
+		panic(err)
+	}
+
+	r := gin.Default()
+	r.GET("/query", func(c *gin.Context) {
+		username := c.Query("username")
+		password := c.Query("password")
+		cookies := c.Query("cookies")
+		roomName := c.Query("roomName")
+
+		req := query.QueryRequest{
+			Username: username,
+			Password: password,
+			Cookies:  cookies,
+			RoomName: roomName,
+		}
+
+		resp, err := queryer.DoQuery(req)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, resp)
+	})
+	r.POST("/query", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		cookies := c.PostForm("cookies")
+		roomName := c.PostForm("roomName")
+
+		req := query.QueryRequest{
+			Username: username,
+			Password: password,
+			Cookies:  cookies,
+			RoomName: roomName,
+		}
+
+		resp, err := queryer.DoQuery(req)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, resp)
+	})
+	r.Run(":8080")
+}
---
+++ query/query.go
@@ -0,0 +1,113 @@
+package query
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/proto"
+)
+
+type Queryer interface {
+	DoQuery(req QueryRequest) (QueryResponse, error)
+}
+
+type QueryRequest struct {
+	Username string
+	Password string
+	Cookies  string
+	RoomName string
+}
+
+type QueryResponse struct {
+	Balance string
+	Power   string
+	Cookies string
+}
+
+type RodQueryer struct {
+	browser *rod.Browser
+	mu      sync.Mutex
+}
+
+func NewRodQueryer(url string) (*RodQueryer, error) {
+	browser := rod.New().ControlURL(url).MustConnect()
+	return &RodQueryer{
+		browser: browser,
+		mu:      sync.Mutex{},
+	}, nil
+}
+
+func (rq *RodQueryer) DoQuery(req QueryRequest) (QueryResponse, error) {
+	rq.mu.Lock()
+	defer rq.mu.Unlock()
+
+	if req.Username == "" || req.Password == "" {
+		if req.Cookies == "" {
+			return QueryResponse{}, fmt.Errorf("missing authentication fields")
+		}
+	}
+
+	if req.RoomName == "" {
+		return QueryResponse{}, fmt.Errorf("missing room name")
+	}
+
+	if req.Cookies != "" {
+		rq.setCookies(req.Cookies)
+	}
+
+	page := rq.browser.MustPage("https://eportal.uestc.edu.cn/qljfwapp/sys/lwUestcDormElecPrepaid/index.do#/record").MustWaitLoad()
+	defer page.Close()
+
+	if page.MustInfo().Title == "Unified identity authentication platform" {
+		// username
+		page.MustElement("#loginViewDiv > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(3)").MustInput(req.Username)
+		// password
+		page.MustElement("#loginViewDiv > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > input:nth-child(3)").MustInput(req.Password)
+		// remember me
+		page.MustElement("#loginViewDiv > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > input:nth-child(1)").MustClick()
+		// login
+		page.MustElement("#loginViewDiv > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > a:nth-child(1)").MustClick()
+
+		// Wait for navigation
+		page.MustWaitLoad()
+	}
+
+	if page.MustInfo().Title != "清水河校区寝室电费充值" {
+		return QueryResponse{}, fmt.Errorf("failed to log in or navigate to the correct page")
+	}
+
+	roomId := "[{\"DORM_ID\":\"" + req.RoomName + "\"}]"
+	resp := page.MustEval(`() => {
+		return fetch('https://eportal.uestc.edu.cn/qljfwapp/sys/lwUestcDormElecPrepaid/dormElecPrepaidMan/queryRoomInfo.do', {
+			method: 'POST',
+			headers: {
+					'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+				},
+				body: 'roomIds=' + encodeURIComponent('` + roomId + `')
+			}).then(response => response.json());
+		}`)
+
+	if resp.Get("0.code").Str() != "0" {
+		return QueryResponse{}, fmt.Errorf("failed to query room info")
+	}
+
+	return QueryResponse{
+		Balance: resp.Get("0.roomInfo.syje").Str(),
+		Power:   resp.Get("0.roomInfo.sydl").Str(),
+		Cookies: rq.getCookies(),
+	}, nil
+}
+
+func (rq *RodQueryer) getCookies() string {
+	cookies := rq.browser.MustGetCookies()
+	cookiesJSON, _ := json.Marshal(cookies)
+	return string(cookiesJSON)
+}
+
+func (rq *RodQueryer) setCookies(cookies string) {
+	var networkCookies []*proto.NetworkCookie
+	json.Unmarshal([]byte(cookies), &networkCookies)
+	rq.browser.MustSetCookies(networkCookies...)
+}
